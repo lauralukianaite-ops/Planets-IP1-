0eab9b9e123c8290261e45ec049fe240c55aab9f
Laura <laura.lukianaite@gmail.com>
2026-02-26 20:29:06 +0200

    Sukūriau klasę ir jos objektą

--- PlanetsIP1.cpp
+++ PlanetsIP1.cpp
@@ -1,11 +1,22 @@
-//testas
+//Planet(name,gravity,moons)
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+//klases aprasymas
+class Planet{
+    public: //public reiskia kad duomenis galima bus pasiekti ir keisti veliau programoje
+        string name;
+        double gravity;
+        int moons;
+};
+
 int main(){
-    string labas = "Labas";
-    cout << labas;
+    Planet p; //objekto instancija
+    p.name = "Earth";
+    p.gravity = 9.81;
+    p.moons = 1;
+    cout << "Success!" <<endl;
     return 0;
 }
