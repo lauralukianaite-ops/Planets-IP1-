f7050401dd9793036eea0976a0b8000751550bed
Laura <laura.lukianaite@gmail.com>
2026-02-26 20:41:28 +0200

    Enkapsuliacija, setter (info įrašymo) ir getter metodai

--- PlanetsIP1.cpp
+++ PlanetsIP1.cpp
@@ -6,17 +6,37 @@ using namespace std;
 
 //klases aprasymas
 class Planet{
-    public: //public reiskia kad duomenis galima bus pasiekti ir keisti veliau programoje
+    private: //tiesiogiai pasiekti duomenis nera gerai, reikia naudoti private
         string name;
         double gravity;
         int moons;
+    public:
+        void setName(string name){
+            this->name = name;
+        }
+        void setGravity(double gravity){
+            this->gravity = gravity;
+        }
+        void setMoons(int moons){
+            this->moons = moons;
+        }
+
+        string getName (){
+            return name;
+        }
+        double getGravity(){
+            return gravity;
+        }
+        int getMoons(){
+            return moons;
+        }
 };
 
 int main(){
     Planet p; //objekto instancija
-    p.name = "Earth";
-    p.gravity = 9.81;
-    p.moons = 1;
+    p.setName("Earth");
+    p.setGravity(9.81);
+    p.setMoons(1);
     cout << "Success!" <<endl;
     return 0;
 }
